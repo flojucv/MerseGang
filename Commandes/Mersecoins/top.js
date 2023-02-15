8e37f0664e986f495a9c0bed7f5f5cc789945539
flojucv <flojucv@gmail.com>
2023-02-15 23:21:16 +0100

    Ajout de la commande de top qui permet de voir les 10 plus gros possesseurs de Mersecoins.  Changes to be committed: 	modified:   .gitignore 	modified:   Commandes/Mersecoins/top.js 	modified:   bdd/link.json

--- Commandes/Mersecoins/top.js
+++ Commandes/Mersecoins/top.js
@@ -1,17 +1,38 @@
 const bddCoins = require("../../bdd/coins.json");
+const bddLink = require("../../bdd/link.json");
+const { EmbedBuilder } = require("discord.js");
 
-module.exports.run = async(client, message, args) => {
-    const listePlusRiche = [];
-    for (const user in bddCoins) {
-        Coins = bddCoins[user];
-        if(listePlusRiche.length === 0) {
-            listePlusRiche.push(user = Coins)
-        } else {
-            for (const riche in listePlusRiche) {
-            
-            }
+function findKeyByValue(obj, value) {
+    for (let key in obj) {
+      if (obj[key] === value) {
+        return key;
+      }
+    }
+    return null;
+  }
+
+module.exports.run = async (client, message, args) => {
+    const sortedObj = Object.fromEntries(
+        Object.entries(bddCoins)
+            .sort(([, val1], [, val2]) => val2 - val1)
+    );
+    
+    const embedRiche = new EmbedBuilder()
+    .setAuthor( {name: "Bot by flojucv", iconURL: "https://media.discordapp.net/attachments/894234723747004426/995231775762690138/logo512x512.png", url: "https://discord.gg/p2QC3NQSmG"} )
+    .setTitle("TOP 10 DES PLUS RICHES FOLLOWERS")
+    .setTimestamp()
+    .setColor("Random")
+    let compteur = 0;
+    const emoji = ["🥇", "🥈", "🥉", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
+    for (const key in sortedObj) {
+        if(compteur < 10) {
+            const coins = sortedObj[key];
+            const discordUser = message.guild.members.cache.find(member => member.id === findKeyByValue(bddLink, key));
+            embedRiche.addFields({ name: `${emoji[compteur]}| ${discordUser === undefined ? `Utilisateur non trouver` : `${discordUser.user.tag}`} (${key})`, value: `${coins} MerseCoins`})
+            compteur ++;
         }
     }
+    message.channel.send({embeds: [embedRiche]});
 }
 
 module.exports.help = {
