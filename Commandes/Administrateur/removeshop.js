fd6ccedf55552db9a0cf75a4c2270a702980844c
flojucv <flojucv@gmail.com>
2023-03-11 22:34:47 +0100

    Patch d'un problème dans la commande pour retirer un item du shop
    Changes to be committed:
    	modified:   Commandes/Administrateur/removeshop.js

--- Commandes/Administrateur/removeshop.js
+++ Commandes/Administrateur/removeshop.js
@@ -10,7 +10,8 @@ module.exports.run = async(client, message, args) => {
 
     if(args[0] < 1 || args[0] > bddShop.length) return message.channel.send(`❌| Votre numéro doit être compris entre 1 et ${bddShop.length}`).then(message => { setTimeout(() => message.delete().catch(err => console.log(err)), 5000); });
 
-    bddShop.splice(args[0], 1);
+    number = parseInt(args[0]) -1;
+    bddShop.splice(number, 1);
     saveBdd("shop", bddShop);
     message.channel.send("✅| L'article a été supprimer").then(message => { setTimeout(() => message.delete().catch(err => console.log(err)), 5000); });
 }
@@ -20,8 +21,9 @@ module.exports.runSlash = async(client, interaction) => {
 
     const article = interaction.options.getNumber("numero");
     if(article > bddShop.length) return interaction.reply({content: `❌| Votre numéro doit être compris entre 1 et ${bddShop.length}`, ephemeral: true});
-
-    bddShop.splice(article, 1);
+    
+    number = parseInt(args[0]) -1;
+    bddShop.splice(number, 1);
     saveBdd("shop", bddShop);
     interaction.reply({content: "✅| L'article a été supprimer", ephemeral: true});
 }
